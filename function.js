de815d78ba280c52457bdaf673c5e072f1193a06
허건 <braveman0823@gmail.com>
2023-12-10 22:46:01 +0900

    함수와 클래스 오브젝트 수업

---
+++ function.js
@@ -0,0 +1,169 @@
+// Function (함수)
+// - fundamental building block in the program
+// - subprogram can be used multiple times
+// - performs a task or calculates a value(값을 계산하기 위해서 존재)
+
+// 1. Function declaration(함수 정의 방법)
+// function name(param1, param2) { body... return; }
+// one function === one thing(하나의 함수는 하나의 일만 하도록 만든다.)
+// naming: doSomething, command, verb(명령, 동사형태로 만들어야함 함수 이름은)
+// e.g. createCardAndPoint -> createCard, createPoint(예시들)
+// function is object in JS(자바에서 함수는 오브젝트이다.)
+function printHello() {
+  console.log('Hello');
+}
+printHello();
+
+function log(message) {
+  console.log(message);
+}
+log('Hello@');
+log(1234);
+
+// 2. Parameters
+// primitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+  obj.name = 'coder';
+}
+const ellie = { name: 'ellie' };
+changeName(ellie);
+console.log(ellie);
+//음 근데 타입스크립트 요즘 안쓴다는데 가르쳐주네 아닌가
+// 3. Default parameters (added in ES6)
+function showMessage(message, from = 'unknown') {
+  console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {//배열형태로 전달된다 ...하면
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+
+  for (const arg of args) {//args의 값들이 순서대로 arg에 넣어준다.
+    console.log(arg);
+  }
+
+  args.forEach((arg) => console.log(arg));
+}
+printAll('dream', 'coding', 'ellie');
+
+// 5. Local scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+  let message = 'hello';
+  console.log(message); // local variable
+  console.log(globalMessage);
+  function printAnother() {
+    console.log(message);
+    let childMessage = 'hello';
+  }
+  // console.log(childMessage); //error
+}
+printMessage();
+
+// 6. Return a value
+function sum(a, b) {
+  return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+  if (user.point > 10) {
+    // long upgrade logic...
+  }
+}
+
+// good
+function upgradeUser(user) {
+  if (user.point <= 10) {
+    return;
+  }
+  // long upgrade logic...
+}
+
+// First-class function
+// functions are treated like any other variable
+// can be assigned as a value to variable
+// can be passed as an argument to other functions.
+// can be returned by another function
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defined. (hoisted)
+// a function expression is created when the execution reaches it.
+const print = function () {
+  // anonymous function(이름없는 함수)
+  console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// 2. Callback function using function expression
+function randomQuiz(answer, printYes, printNo) {
+  if (answer === 'love you') {
+    printYes();
+  } else {
+    printNo();
+  }
+}
+// anonymous function
+const printYes = function () {
+  console.log('yes!');
+};
+
+// named function
+// better debugging in debugger's stack traces(디버깅에서 더 좋음)
+// recursions
+const printNo = function print() {
+  console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+// const simplePrint = function () {
+//   console.log('simplePrint!');
+// };
+
+const simplePrint = () => console.log('simplePrint!');
+const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
+  // do something more
+  return a * b;
+};
+
+// IIFE: Immediately Invoked Function Expression
+(function hello() {
+  console.log('IIFE');
+})();//바로 함수 호출이 가능해짐
+
+// Fun quiz time❤️
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, remainder
+
+function calculate(command, a, b) {
+  switch (command) {
+    case 'add':
+      return a + b;
+    case 'substract':
+      return a - b;
+    case 'divide':
+      return a / b;
+    case 'multiply':
+      return a * b;
+    case 'remainder':
+      return a % b;
+    default:
+      throw Error('unknown command');
+  }
+}
+console.log(calculate('add', 2, 3));
